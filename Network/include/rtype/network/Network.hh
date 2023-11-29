9b87481fa75b96fbc9ac6f7a2392a4638c77cb72
Luka Camus <luka.camus@epitech.eu>
2023-11-29 12:05:04 +0100

    [ADD] Network => structure for transmitting infos, is prone to changes

---
+++ Network/include/rtype/network/Network.hh
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2023
+** network
+** File description:
+** Network
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace ntw
+{
+    enum NetworkType {
+        Start,
+        None,
+    };
+
+    struct Communication {
+        public:
+            NetworkType type{None};
+            std::vector<std::string> args{};
+    };
+} // namespace ntw
