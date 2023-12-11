7f4d92f7e709f1507a1490c17a5c88d2656d54b2
Guillaume Lebreton <guillaume.lebreton@epitech.eu>
2023-12-11 11:31:00 +0100

    [ADD] Graphics folder for the generation of the dynamic library of the SFML

---
+++ GameEngine/Graphics/include/Manager/SceneManager.hpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2023
+** rtype
+** File description:
+** sceneManager
+*/
+
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+class Scene {
+public:
+    virtual void init() = 0;
+    virtual void handleInput() = 0;
+    virtual void update(float dt) = 0;
+    virtual void draw(sf::RenderWindow& window) = 0;
+};
+
+class SceneManager {
+public:
+    SceneManager();
+
+    void addScene(const std::string& name, std::shared_ptr<Scene> scene);
+    void removeScene(const std::string& name);
+    void changeScene(const std::string& name);
+    void updateCurrentScene(float dt);
+    void drawCurrentScene(sf::RenderWindow& window);
+
+private:
+    std::unordered_map<std::string, std::shared_ptr<Scene>> scenes;
+    std::shared_ptr<Scene> currentScene;
+};
---
+++ GameEngine/Graphics/src/Manager/TextureManager.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2023
+** rtype
+** File description:
+** textureManager
+*/
+
+#include <include/Manager/TextureManager.hpp>
+
+TextureManager::TextureManager() {
+}
+
+bool TextureManager::loadTexture(const std::string& name, const std::string& filename) {
+    sf::Texture texture;
+    if (!texture.loadFromFile(filename)) {
+        return false;
+    }
+
+    textures[name] = std::move(texture);
+    return true;
+}
+
+sf::Texture& TextureManager::getTexture(const std::string& name) {
+    return textures.at(name);
+}
+
+void TextureManager::removeTexture(const std::string& name) {
+    textures.erase(name);
+}
