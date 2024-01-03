ef6b75f4fbeedad2f6623d0ad4f7d8d41f22cb1f
melissa-laget <melissa.laget@epitech.eu>
2024-01-03 17:24:58 +0100

    [MODIFY] menu to work better with clicks

--- Client/src/menu.cpp
+++ Client/src/menu.cpp
@@ -22,6 +22,7 @@ void rclient::Menu::menu()
     SparseArray<rtype::TransformComponent> transforms{};
     SparseArray<rtype::TagComponent> tags{};
     SparseArray<rtype::BoxColliderComponent> colliders{};
+
     sprite_system.loadTexture("BG", "./Client/assets/backgrounds/024.png");
     sprite_system.loadTexture("START", "./Client/assets/menu_assets/START.png");
     sprite_system.loadTexture("mute_button", "./Client/assets/menu_assets/MUTE.png");
@@ -37,36 +38,36 @@ void rclient::Menu::menu()
     const Entity unmute_button{manager.create_entity()};
     const Entity quit_button{manager.create_entity()};
 
-    constexpr auto rect_size_width = 153;
-    constexpr auto rect_size_height = 48;
-    constexpr auto background_width = 800;
-    constexpr auto background_height = 600;
+    const sf::Vector2u initial_window_size = le_client.get_window_size();
 
     sprites.emplace_at(background, sprite_system.createSprite("BGSprite", "BG"),
-                       sf::Rect<int>{0, 0, background_width, background_height});
+                       sf::Rect<int>{0, 0, static_cast<int>(initial_window_size.x),
+                                     static_cast<int>(initial_window_size.y)});
+
     sprites.emplace_at(start_button, sprite_system.createSprite("STARTSprite", "START"),
-                       sf::Rect<int>{0, 0, rect_size_width, rect_size_height});
+                       sf::Rect<int>{0, 0, static_cast<int>(initial_window_size.x / 4.0),
+                                     static_cast<int>(initial_window_size.y / 4.0)});
+
     sprites.emplace_at(mute_button, sprite_system.createSprite("mute_buttonSprite", "mute_button"),
-                       sf::Rect<int>{0, 0, rect_size_width, rect_size_height});
+                       sf::Rect<int>{0, 0, static_cast<int>(initial_window_size.x / 4.0),
+                                     static_cast<int>(initial_window_size.y / 4.0 + 100)});
     sprites.emplace_at(unmute_button,
                        sprite_system.createSprite("unmute_buttonSprite", "unmute_button"),
-                       sf::Rect<int>{0, 0, rect_size_width, rect_size_height});
+                       sf::Rect<int>{0, 0, static_cast<int>(initial_window_size.x / 4.0),
+                                     static_cast<int>(initial_window_size.y / 4.0 + 200)});
     sprites.emplace_at(quit_button, sprite_system.createSprite("quit_buttonSprite", "quit_button"),
-                       sf::Rect<int>{0, 0, rect_size_width, rect_size_height});
+                       sf::Rect<int>{0, 0, static_cast<int>(initial_window_size.x / 4.0),
+                                     static_cast<int>(initial_window_size.y / 4.0 + 300)});
 
-    const sf::Vector2u start_window_size = le_client.get_window_size();
-
-    const auto x_coord = start_window_size.x / 4.0;
-    const auto y_start = start_window_size.y - 500.0;
-    const auto y_mute = start_window_size.y - 400.0;
-    const auto y_unmute = start_window_size.y - 300.0;
-    const auto y_quit = start_window_size.y - 200.0;
-    constexpr auto scale = 2.0F;
     transforms.emplace_at(background, 0.0F, 0.0F, 0.0F, 0.0F, 1.F, 1.F);
-    transforms.emplace_at(start_button, x_coord, y_start, 0.0F, 0.0F, scale, scale);
-    transforms.emplace_at(mute_button, x_coord, y_mute, 0.0F, 0.0F, scale, scale);
-    transforms.emplace_at(unmute_button, x_coord, y_unmute, 0.0F, 0.0F, scale, scale);
-    transforms.emplace_at(quit_button, x_coord, y_quit, 0.0F, 0.0F, scale, scale);
+    transforms.emplace_at(start_button, initial_window_size.x / 4.0, initial_window_size.y / 4.0,
+                          0.0F, 0.0F, 2.0F, 2.0F);
+    transforms.emplace_at(mute_button, initial_window_size.x / 4.0,
+                          initial_window_size.y / 4.0 + 100, 0.0F, 0.0F, 2.0F, 2.0F);
+    transforms.emplace_at(unmute_button, initial_window_size.x / 4.0,
+                          initial_window_size.y / 4.0 + 200, 0.0F, 0.0F, 2.0F, 2.0F);
+    transforms.emplace_at(quit_button, initial_window_size.x / 4.0,
+                          initial_window_size.y / 4.0 + 300, 0.0F, 0.0F, 2.0F, 2.0F);
 
     tags.emplace_at(background, "background");
     tags.emplace_at(start_button, "start_button");
@@ -91,25 +92,33 @@ void rclient::Menu::menu()
         le_client.read_input();
 
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            const sf::Vector2u window_size = le_client.get_window_size();
-            const sf::Vector2f position = le_client.get_window().mapPixelToCoords(
-                sf::Mouse::getPosition(le_client.get_window()));
-            const auto x_value_max = static_cast<float>(window_size.x / 4.0);
-            const auto x_value_min = static_cast<float>(window_size.x / 2.0) + 76.0;
-
-            if (position.x >= x_value_max && position.x <= x_value_min &&
-                position.y >= window_size.y - 475.0 && position.y <= window_size.y - 400.0)
+            const auto window_size = le_client.get_window_size();
+            const auto mouse_position = sf::Mouse::getPosition(le_client.get_window());
+            if (mouse_position.x >= window_size.x / 4.0 &&
+                mouse_position.x <= window_size.x / 4.0 + 306.0 &&
+                mouse_position.y >= window_size.y / 4.0 &&
+                mouse_position.y <= window_size.y / 4.0 + 96.0) {
                 continue;
-            // TODO: Start game
-            if (position.x >= x_value_max && position.y <= x_value_min &&
-                position.y >= window_size.y - 375.0 && position.y <= window_size.y - 300.0)
+                // TODO: Start game
+            }
+            if (mouse_position.x >= window_size.x / 4.0 &&
+                mouse_position.x <= window_size.x / 4.0 + 306.0 &&
+                mouse_position.y >= window_size.y / 4.0 + 100 &&
+                mouse_position.y <= window_size.y / 4.0 + 196.0) {
                 audio_system.setVolume("music", 0);
-            if (position.x >= x_value_max && position.x <= x_value_min &&
-                position.y >= window_size.y - 275.0 && position.y <= window_size.y - 200.0)
+            }
+            if (mouse_position.x >= window_size.x / 4.0 &&
+                mouse_position.x <= window_size.x / 4.0 + 306.0 &&
+                mouse_position.y >= window_size.y / 4.0 + 200 &&
+                mouse_position.y <= window_size.y / 4.0 + 296.0) {
                 audio_system.setVolume("music", 100);
-            if (position.x >= x_value_max && position.x <= x_value_min &&
-                position.y >= window_size.y - 175.0 && position.y <= window_size.y - 100.0)
+            }
+            if (mouse_position.x >= window_size.x / 4.0 &&
+                mouse_position.x <= window_size.x / 4.0 + 306.0 &&
+                mouse_position.y >= window_size.y / 4.0 + 300 &&
+                mouse_position.y <= window_size.y / 4.0 + 396.0) {
                 le_client.close_window();
+            }
         }
     }
 }
